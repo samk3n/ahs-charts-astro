e220a530c15e4f818e41578d6eed9969c73b49a8
samk3n <neubeck.theo@gmail.com>
2025-10-27 16:43:49 +0100

    Added link to privacy and terms pages in the footer

--- src/pages/api/oauth/google.ts
+++ src/pages/api/oauth/google.ts
@@ -15,7 +15,15 @@ export const GET: APIRoute = async (ctx) => {
     if (error || !data?.url) {
         const url = new URL('/auth/login', ctx.url)
         url.searchParams.set('e', encodeURIComponent(error?.message || 'OAuth failed'))
-        return Response.redirect(url, 303)
+        // return Response.redirect(url, 303)
+        return new Response(null, {
+            status: 303,
+            headers: new Headers({ Location: url.toString() }),
+        })
     }
-    return Response.redirect(data.url, 303)
+    // return Response.redirect(data.url, 303)
+    return new Response(null, {
+        status: 303,
+        headers: new Headers({ Location: data.url})
+    })
 }
